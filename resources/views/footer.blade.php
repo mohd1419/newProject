3eeb56346c41b1bfa97be53c78081a33bcea923f
codingtheories <codingtheories@gmail.com>
2021-06-17 22:30:10 +0300

    git init

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,47 @@
+<!-- jQuery -->
+<script src="assets/vendor/jquery.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="assets/vendor/popper.min.js"></script>
+<script src="assets/vendor/bootstrap.min.js"></script>
+
+<!-- Perfect Scrollbar -->
+<script src="assets/vendor/perfect-scrollbar.min.js"></script>
+
+<!-- MDK -->
+<script src="assets/vendor/dom-factory.js"></script>
+<script src="assets/vendor/material-design-kit.js"></script>
+
+<!-- App JS -->
+<script src="assets/js/app.js"></script>
+
+<!-- Highlight.js -->
+<script src="assets/js/hljs.js"></script>
+
+<!-- App Settings (safe to remove) -->
+<script src="assets/js/app-settings.js"></script>
+
+<!-- Global Settings -->
+<script src="assets/js/settings.js"></script>
+
+<!-- Moment.js -->
+<script src="assets/vendor/moment.min.js"></script>
+<script src="assets/vendor/moment-range.js"></script>
+
+<!-- Chart.js -->
+<script src="assets/vendor/Chart.min.js"></script>
+<script src="assets/js/chartjs-rounded-bar.js"></script>
+<script src="assets/js/chartjs.js"></script>
+
+<!-- Instructor Dashboard Page JS -->
+<script src="assets/js/page.instructor-dashboard.js"></script>
+
+<!-- List.js -->
+<script src="assets/vendor/list.min.js"></script>
+<script src="assets/js/list.js"></script>
+
+</body>
+
+
+<!-- Mirrored from learnplus.demo.frontendmatter.com/instructor-dashboard.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 17 Jun 2021 18:44:07 GMT -->
+</html>
